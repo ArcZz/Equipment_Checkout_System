02020e572ff3c697a700fa664c7e02fbdc83f613
FireLemons <shenyang64@gmail.com>
2016-05-11 10:35:39 -0500

    adding tags are forced to be lowercase

--- addCategory.php
+++ addCategory.php
@@ -9,7 +9,7 @@
 	
 	if($stmt = mysqli_prepare($link, "INSERT INTO item_category (name) VALUES (?)") or die ("prepare error" . mysqli_error($link))){
 			
-		mysqli_stmt_bind_param($stmt, "s", $category) or die ("bind param" . mysqli_stmt_error($stmt));
+		mysqli_stmt_bind_param($stmt, "s", strtolower($category)) or die ("bind param" . mysqli_stmt_error($stmt));
 		mysqli_stmt_execute($stmt) or die(mysqli_stmt_error($stmt));
 			
 		if(mysqli_affected_rows($link)){
